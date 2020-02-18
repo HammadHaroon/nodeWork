870afdcae5e8d12a76e77bfdb87f2f05a01d97c6
HammadHaroon <52397975+HammadHaroon@users.noreply.github.com>
2020-02-18 11:33:26 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs=require('fs')
+
+http.createServer(function(req, res) {
+    fs.readFile('sampleFile.txt','utf8',function(err,data){
+        res.writeHead(200, {'Content-Type': 'text'});
+        res.write(data);
+        res.end();
+    });
+}).listen(4000);
